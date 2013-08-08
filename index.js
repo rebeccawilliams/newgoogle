1efbf4e91354ff68543739297f828d60249fc51f
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 01:01:54 -0700

    aoeuaoeu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var memoize = require('memoizee')
 var search  = require('./search.js')
 
-var socrata = memoize(search.socrata, {async:true})
+exports.socrata = memoize(search.socrata, {async:true})
--- search.js
+++ search.js
@@ -1,9 +1,11 @@
-var d3 = require('d3')
+var https = require('https')
 
 exports.socrata = function(terms, portal, page, callback) {
-  var url = 'https://' + portal + '/api/search/views.json?limit=1&page=' + page + '&q=' + encodeURIComponent(terms)
-  d3.json(url, function(result) {
-    var view = result.results[0].view
+  var url = 'https://' + portal + '/api/search/views.json?limit=1&page=' + page + '&q=' + encodeURIComponent(terms);
+  https.get(url, function(response) {
+    console.log(response.statusCode)
+    var data = JSON.parse(response.message)
+    var view = data.results[0].view
     delete view.columns
     callback(view)
   })
