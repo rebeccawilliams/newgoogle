3f803ba71e04865b7b48af855b5f3398c0aace91
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 01:07:58 -0700

    jquery

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var memoize = require('memoizee')
-var search  = require('./search.js')
+var $ = require('jquery')
 
-exports.socrata = memoize(search.socrata, {async:true})
+socrata = function(terms, portal, page, callback) {
+  var url = 'https://' + portal + '/api/search/views.json?limit=1&page=' + page + '&q=' + encodeURIComponent(terms);
+  $.get(url, function(data) {
+    var view = data.results[0].view
+    delete view.columns
+    callback(view)
+  })
+}
+
+exports.socrata = memoize(socrata, {async:true})
