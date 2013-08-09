0361d22192ee8154fd9711ca3932cb6d47da6bf4
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 19:14:32 -0700

    _

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var request = require('browser-request')
 
-socrata = function(terms, portal, page, callback) {
+exports.socrata = function(terms, portal, page, callback) {
   var url = 'https://' + portal + '/api/search/views.json?limit=1&page=' + page + '&q=' + encodeURIComponent(terms);
-  request.get(url, function(data) {
-    var view = data.results[0].view
+  request(url, function(err, res, body) {
+    var view = JSON.parse(body).results[0].view
     delete view.columns
-    callback(view)
+    return callback(view)
   })
 }
 
-exports.socrata = memoize(socrata, {async:true})
+
+/*
+exports.socrata('elevator', 'data.cityofnewyork.us', 1, function(view) {
+})
+*/
+
+window.openprism = exports
