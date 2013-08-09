3b55d0c83512c3973c94f3518ad73e028af8e1b3
Thomas Levine <occurrence@thomaslevine.com>
2013-08-09 08:51:23 -0700

    get ckan domains

--- src/ckan.js
+++ src/ckan.js
@@ -9,3 +9,22 @@ function group(callback) {
     callback(JSON.parse(body).packages)
   })
 }
+
+function dataset(id, callback) {
+  request('http://datacatalogs.org/api/rest/dataset/' + id, function(err, res, body) {
+    var dataset = JSON.parse(body)
+    callback(dataset)
+  })
+}
+
+function main() {
+  group(function(packages) {
+    packages.map(function(p) {
+      dataset(p, function(metadata) {
+        console.log(metadata.url)
+      })
+    })
+  })
+}
+
+main()
